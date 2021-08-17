f865e08f3df8b542ef1bda074998896acc86581c
Amit8830 <gamit9022@gmail.com>
2021-08-17 14:09:51 +0530

    initial commit

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,304 +1,314 @@
-import rasmalai from '../../images/rasmalai.png';
-import choco from '../../images/choco.png';
-import alljar from '../../images/alljar.png';
-import cake3 from '../../images/cake3.png';
-import cake1 from '../../images/cake1.png';
-import cake2 from '../../images/cake2.png';
-import cake4 from '../../images/cake4.png';
-import cake5 from '../../images/cake5.png';
-import cake6 from '../../images/cake6.png';
-import cake7 from '../../images/cake7.png';
-import cake8 from '../../images/cake8.png';
-import cake9 from '../../images/cake9.png';
-import cake10 from '../../images/cake10.png';
-import cake11 from '../../images/cake11.png';
-import cake12 from '../../images/cake12.png';
-import cake13 from '../../images/cake13.png';
-import cake14 from '../../images/cake14.png';
-import cake15 from '../../images/cake15.png';
-import cake16 from '../../images/cake16.png';
-import cake17 from '../../images/cake17.png';
-import cake18 from '../../images/cake18.png';
-import cake19 from '../../images/cake19.png';
-import cake20 from '../../images/cake20.png';
-import cake21 from '../../images/cake21.png';
-import cake22 from '../../images/cake22.png';
-import cup1 from '../../images/cup1.png';
-import cup2 from '../../images/cup2.png';
-import cup3 from '../../images/cup3.png';
-import cup4 from '../../images/cup4.png';
-import cup5 from '../../images/cup5.png';
-import cup6 from '../../images/cup6.png';
-import cup7 from '../../images/cup7.png';
-import cup8 from '../../images/cup8.png';
-import menucard from '../../images/menucard.png';
-import menucard1 from '../../images/menucard1.png';
-import menucard2 from '../../images/menucard2.png';
-import bag from "../../images/bag.png"
-import bar from "../../images/bar.png"
-import smash from "../../images/samash.png"
-import name from "../../images/name.png"
-import walnut from "../../images/wal.png"
-import bro from "../../images/bro.png"
-
-
-
+import rasmalai from "../../images/rasmalai.png";
+import choco from "../../images/choco.png";
+import alljar from "../../images/alljar.png";
+import cake3 from "../../images/cake3.png";
+import cake1 from "../../images/cake1.png";
+import cake2 from "../../images/cake2.png";
+import cake4 from "../../images/cake4.png";
+import cake5 from "../../images/cake5.png";
+import cake6 from "../../images/cake6.png";
+import cake7 from "../../images/cake7.png";
+import cake8 from "../../images/cake8.png";
+import cake9 from "../../images/cake9.png";
+import cake10 from "../../images/cake10.png";
+import cake11 from "../../images/cake11.png";
+import cake12 from "../../images/cake12.png";
+import cake13 from "../../images/cake13.png";
+import cake14 from "../../images/cake14.png";
+import cake15 from "../../images/cake15.png";
+import cake16 from "../../images/cake16.png";
+import cake17 from "../../images/cake17.png";
+import cake18 from "../../images/cake18.png";
+import cake19 from "../../images/cake19.png";
+import cake20 from "../../images/cake20.png";
+import cake21 from "../../images/cake21.png";
+import cake22 from "../../images/cake22.png";
+import cup1 from "../../images/cup1.png";
+import cup2 from "../../images/cup2.png";
+import cup3 from "../../images/cup3.png";
+import cup4 from "../../images/cup4.png";
+import cup5 from "../../images/cup5.png";
+import cup6 from "../../images/cup6.png";
+import cup7 from "../../images/cup7.png";
+import cup8 from "../../images/cup8.png";
+import menucard from "../../images/menucard.png";
+import menucard1 from "../../images/menucard1.png";
+import menucard2 from "../../images/menucard2.png";
+import bag from "../../images/bag.png";
+import bar from "../../images/bar.png";
+import smash from "../../images/samash.png";
+import name from "../../images/name.png";
+import walnut from "../../images/wal.png";
+import bro from "../../images/bro.png";
+import cone from "../../images/cone.png";
+import cir from "../../images/cir.png";
 
 export const productData = [
   {
     img: menucard,
-     alt: 'Pizza',
-     name: 'CupCake MenuCard',
-     button: 'Order Now'
-   },
-   {
-    img: menucard1,
-     alt: 'Pizza',
-     name: 'Cake Jar MenuCard',
-     button: 'Order Now'
-   },
-   {
-    img: menucard2,
-     alt: 'Pizza',
-     name: 'Cake MenuCard',
-     button: 'Order Now'
-   },
+    alt: "Pizza",
+    name: "CupCake MenuCard",
+    button: "Order Now",
+  },
   {
-   img: bar,
-    alt: 'Pizza',
-    name: 'Chocolate Bar',
-    button: 'Order Now'
+    img: menucard1,
+    alt: "Pizza",
+    name: "Cake Jar MenuCard",
+    button: "Order Now",
+  },
+  {
+    img: menucard2,
+    alt: "Pizza",
+    name: "Cake MenuCard",
+    button: "Order Now",
+  },
+  {
+    img: bar,
+    alt: "Pizza",
+    name: "Chocolate Bar",
+    button: "Order Now",
   },
   {
     img: bag,
-     alt: 'Pizza',
-     name: 'Chocolate Bag',
-     button: 'Order Now'
-   },
-   {
+    alt: "Pizza",
+    name: "Chocolate Bag",
+    button: "Order Now",
+  },
+  {
     img: bro,
-     alt: 'Pizza',
-     name: 'Chocolate Brownie',
-     button: 'Order Now'
-   },
-   {
+    alt: "Pizza",
+    name: "Chocolate Brownie",
+    button: "Order Now",
+  },
+  {
+    img: cir,
+    alt: "Pizza",
+    name: " Rakshabandhan Chocolates",
+    button: "Order Now",
+  },
+  {
+    img: cone,
+    alt: "Pizza",
+    name: "Chocolate Cones",
+    button: "Order Now",
+  },
+  {
     img: name,
-     alt: 'Pizza',
-     name: 'Name Cake',
-     button: 'Order Now'
-   },
-   {
+    alt: "Pizza",
+    name: "Name Cake",
+    button: "Order Now",
+  },
+  {
     img: smash,
-     alt: 'Pizza',
-     name: 'Heart Shape Smash Cake',
-     button: 'Order Now'
-   },
-   {
+    alt: "Pizza",
+    name: "Heart Shape Smash Cake",
+    button: "Order Now",
+  },
+  {
     img: walnut,
-     alt: 'Pizza',
-     name: 'Walnut Brownie',
-     button: 'Order Now'
-   },
-   {
+    alt: "Pizza",
+    name: "Walnut Brownie",
+    button: "Order Now",
+  },
+  {
     img: rasmalai,
-     alt: 'Pizza',
-     name: 'Rasmalai Jar',
-     button: 'Order Now'
-   },
+    alt: "Pizza",
+    name: "Rasmalai Jar",
+    button: "Order Now",
+  },
   {
     img: alljar,
-    alt: 'Pizza',
-    name: 'New Chocolate Jar',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "New Chocolate Jar",
+    button: "Order Now",
   },
   {
     img: choco,
-    alt: 'Pizza',
-    name: 'ChocoChips Jar',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "ChocoChips Jar",
+    button: "Order Now",
   },
   {
     img: cup1,
-    alt: 'Pizza',
-    name: 'Cupcake',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "Cupcake",
+    button: "Order Now",
   },
   {
     img: cup2,
-    alt: 'Pizza',
-    name: 'Cupcake',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "Cupcake",
+    button: "Order Now",
   },
   {
     img: cup3,
-    alt: 'Pizza',
-    name: 'Cupcake',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "Cupcake",
+    button: "Order Now",
   },
   {
     img: cup4,
-    alt: 'Pizza',
-    name: 'Cupcake',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "Cupcake",
+    button: "Order Now",
   },
   {
     img: cup5,
-    alt: 'Pizza',
-    name: 'Cupcake',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "Cupcake",
+    button: "Order Now",
   },
   {
     img: cup6,
-    alt: 'Pizza',
-    name: 'Cupcake',
-    button: 'Order Now'
-  }, {
+    alt: "Pizza",
+    name: "Cupcake",
+    button: "Order Now",
+  },
+  {
     img: cup7,
-    alt: 'Pizza',
-    name: 'Cupcake',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "Cupcake",
+    button: "Order Now",
   },
   {
     img: cup8,
-    alt: 'Pizza',
-    name: 'Cupcake',
-    button: 'Order Now'
+    alt: "Pizza",
+    name: "Cupcake",
+    button: "Order Now",
   },
   {
     img: cake22,
-    alt: 'Brownie',
-    name: 'Rakhi gift chocalate bar 👫',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Rakhi gift chocalate bar 👫",
+    button: "Order Now",
   },
-
 ];
-
 
 export const productDataTwo = [
   {
     img: cake1,
-    alt: 'Donuts',
-    name: 'KitKat Cake 🍫🎂',
-    button: 'Order Now'
+    alt: "Donuts",
+    name: "KitKat Cake 🍫🎂",
+    button: "Order Now",
   },
   {
     img: cake2,
-    alt: 'Ice Cream',
-    name: 'Fully Chocalate overloaded cake ❤️',
-    button: 'Order Now'
+    alt: "Ice Cream",
+    name: "Fully Chocalate overloaded cake ❤️",
+    button: "Order Now",
   },
   {
     img: cake3,
-    alt: 'Brownie',
-    name: 'Black forest cake 🎂❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Black forest cake 🎂❤️",
+    button: "Order Now",
   },
   {
     img: cake4,
-    alt: 'Brownie',
-    name: 'Chocalate message box ❤️ ',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Chocalate message box ❤️ ",
+    button: "Order Now",
   },
   {
     img: cake5,
-    alt: 'Brownie',
-    name: 'Gulkand cake 🎂❤️',
-    button: 'Order Now'
-   },
+    alt: "Brownie",
+    name: "Gulkand cake 🎂❤️",
+    button: "Order Now",
+  },
   {
     img: cake6,
-    alt: 'Brownie',
-    name: 'Variety of cupcakes ❤️🧁',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Variety of cupcakes ❤️🧁",
+    button: "Order Now",
   },
   {
     img: cake7,
-    alt: 'Brownie',
-    name: 'Dry moist cake ❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Dry moist cake ❤️",
+    button: "Order Now",
   },
   {
     img: cake8,
-    alt: 'Brownie',
-    name: 'Brownies ❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Brownies ❤️",
+    button: "Order Now",
   },
   {
     img: cake9,
-    alt: 'Brownie',
-    name: 'Special 3 Tier cake ❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Special 3 Tier cake ❤️",
+    button: "Order Now",
   },
   {
     img: cake10,
-    alt: 'Brownie',
-    name: 'Kulfi falooda cake ❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Kulfi falooda cake ❤️",
+    button: "Order Now",
   },
   {
     img: cake11,
-    alt: 'Brownie',
-    name: 'Chocalate overloaded cake 🎂💓',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Chocalate overloaded cake 🎂💓",
+    button: "Order Now",
   },
   {
     img: cake12,
-    alt: 'Brownie',
-    name: ' Special Cake',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: " Special Cake",
+    button: "Order Now",
   },
   {
     img: cake13,
-    alt: 'Brownie',
-    name: 'Chocolate Message box 💓',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Chocolate Message box 💓",
+    button: "Order Now",
   },
   {
     img: cake14,
-    alt: 'Brownie',
-    name: 'Give a special gift to your special person ❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Give a special gift to your special person ❤️",
+    button: "Order Now",
   },
   {
     img: cake15,
-    alt: 'Brownie',
-    name: '❤️ Strawberry cake 🎂',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "❤️ Strawberry cake 🎂",
+    button: "Order Now",
   },
   {
     img: cake16,
-    alt: 'Brownie',
-    name: 'Baby shower cake ❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Baby shower cake ❤️",
+    button: "Order Now",
   },
   {
     img: cake17,
-    alt: 'Brownie',
-    name: 'Rakhi gift chocalate bar 👫. ',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "Rakhi gift chocalate bar 👫. ",
+    button: "Order Now",
   },
   {
     img: cake18,
-    alt: 'Brownie',
-    name: 'CHARACTER SLIDER BOX❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "CHARACTER SLIDER BOX❤️",
+    button: "Order Now",
   },
   {
     img: cake19,
-    alt: 'Brownie',
-    name: 'CHOCOLATE POUCHE ❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "CHOCOLATE POUCHE ❤️",
+    button: "Order Now",
   },
   {
     img: cake20,
-    alt: 'Brownie',
-    name: 'MINI HAMPER BOX ❤️',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "MINI HAMPER BOX ❤️",
+    button: "Order Now",
   },
   {
     img: cake21,
-    alt: 'Brownie',
-    name: 'SPECIAL PEACOCK DESIGN BOX 🦚',
-    button: 'Order Now'
+    alt: "Brownie",
+    name: "SPECIAL PEACOCK DESIGN BOX 🦚",
+    button: "Order Now",
   },
 ];
